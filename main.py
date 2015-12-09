48cae259134261cad7fc1f446a2493187fee7a6d
TheStoof <neyens.s@gmail.com>
2015-12-08 22:17:13 -0500

    Added simple menu.

--- main.py
+++ main.py
@@ -2,52 +2,71 @@ import os
 from model import comic
 import comic_db
 
+comic_database = None
+
 def fileExists(filename):
     exists = os.path.isfile(filename)
     print("Checking if {filename} exists...{exists}".format(filename=filename, exists=exists))
     return exists
 
+def get_record_from_user():
+    universe = input("Universe: ")
+    title = input("Title: ")
+    serial = input("Serial: ")
+    comic_database.add_record(universe, title, serial)
+
+def menu():
+    menu_items = [
+        {
+            "display": "Add new record",
+            "function": get_record_from_user,
+            "parameters": None
+        },
+        {
+            "display": "Display all records",
+            "function": print_records,
+            "parameters": comic_database.find_all_records
+        },
+        {
+            "display": "Exit",
+            "function": exit,
+            "parameters": None
+        }
+    ]
+
+    while True:
+        for index, item in enumerate(menu_items):
+            print('{option}: {display}'.format(option=index+1, display=item["display"]))
+
+        print()
+        choice = input("Choice: ")
+        choice = int(choice)
+        choice = -1 if choice < 1 else choice - 1
+
+        if choice < 0 or choice >= len(menu_items):
+            pass
+        else:
+            if (menu_items[choice]["parameters"] is not None):
+                menu_items[choice]["function"](
+                    menu_items[choice]["parameters"]()
+                )
+            else:
+                menu_items[choice]["function"]()
+
+def null_func():
+    pass
+
+def print_records(method):
+    records = method
+    for record in records:
+        print(record)
+
 def main():
-
-    comic_database = comic_db.ComicDatabase("comics.db")
-
     if not fileExists(comic_database.get_comic_database_filename()):
         comic_database.initializeDatabase()
     else:
-
-        add = input("Add record? (y/n): ")
-        if (add == 'y'):
-            universe = input("Universe: ")
-            title = input("Title: ")
-            serial = input("Serial: ")
-            comic_database.add_record(universe, title, serial)
-
-        print("Getting all records...")
-        all_records = comic_database.find_all_records()
-
-        for record in all_records:
-            print(record)
-
-        print()
-        print("Getting all DC Universe records...")
-        records = comic_database.find_by_universe(["DC"])
-
-        for record in records:
-            print(record)
-
-        print()
-        print("Getting all titles containing 'Superman'")
-        records = comic_database.find_by_title(["%Superman%"])
-
-        for record in records:
-            print(record)
-
-        print()
-        print("Getting all serials with 0001...")
-        records = comic_database.find_by_serial(["0001"])
-
-        for record in records:
-            print(record)
+        menu()
 
 if __name__ == "__main__":
+    comic_database = comic_db.ComicDatabase("comics.db")
     main()
